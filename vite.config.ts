547496b455727ecf65904c9c57fde752b8d59ae7
DoceAzedo <me@doceazedo.com>
2026-08-31 19:40:56 -0300

    update paraglide rules

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 			project: "./project.inlang",
 			outdir: "./src/lib/paraglide",
 			emitTsDeclarations: true,
-			strategy: ["localStorage", "cookie", "url", "baseLocale"],
+			strategy: ["url", "localStorage", "cookie", "baseLocale"],
 		}),
 	],
 });
